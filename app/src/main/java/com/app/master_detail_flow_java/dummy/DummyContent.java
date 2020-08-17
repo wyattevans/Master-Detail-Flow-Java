9224e508ff31b41bac640798d838766b53760048
Wyatt Evans <syattevans@gmail.com>
2020-08-16 20:37:54 -0600

    Cleaned up DummyContent.

--- app/src/main/java/com/app/master_detail_flow_java/dummy/DummyContent.java
+++ app/src/main/java/com/app/master_detail_flow_java/dummy/DummyContent.java
@@ -5,28 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
 public class DummyContent {
 
-    /**
-     * An array of sample (dummy) items.
-     */
     public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
 
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
     public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
 
     private static final int COUNT = 25;
 
     static {
-        // Add some sample items.
         for (int i = 1; i <= COUNT; i++) {
             addItem(createDummyItem(i));
         }
@@ -50,15 +37,12 @@ public class DummyContent {
         return builder.toString();
     }
 
-    /**
-     * A dummy item representing a piece of content.
-     */
     public static class DummyItem {
         public final String id;
         public final String content;
         public final String details;
 
-        public DummyItem(String id, String content, String details) {
+        DummyItem(String id, String content, String details) {
             this.id = id;
             this.content = content;
             this.details = details;
